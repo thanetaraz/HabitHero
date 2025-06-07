b0c9142cbe1320ed4d247c54c83719835b74e318
thanet <98311629+thanetaraz@users.noreply.github.com>
2025-06-07 22:13:04 +0700

    🚀 feat: เพิ่ม UI หน้าแรกของ Habithero

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>Content Inside the About Page</div>;
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
-import Image from "next/image";
+import React from "react";
+import { PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 
-export default function Home() {
+const Page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="text-red-400 bg-gray-50 h-screen">
+      <div className="w-4xl mx-auto bg-white rounded-xl shadow-sm border border-gray-200 p-6 mb-6">
+        <section className="text-black flex justify-between">
+          <div className="mt-4">
+            <h1 className="font-bold text-gray-900">Habit Tracker</h1>
+            <p className="text-gray-500 mb-4">
+              เปลี่ยนนิสัยเพื่อเป็นคนที่ดีกว่าเดิม
+            </p>
+            <div className="flex gap-2">
+              <button
+                type="button"
+                className="shadow-md bg-blue-100 flex items-center gap-2 border-2 border-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                <span className="w-4 h-4 bg-green-500 rounded-full"></span>
+                <span className="text-blue-700">อ่านหนังสือ 30 นาที</span>
+                <TrashIcon className="h-4 w-4 text-gray-500" />
+              </button>
+              <button
+                type="button"
+                className="shadow-md flex items-center gap-2 border-2 bg-white border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                <span className="w-4 h-4 bg-purple-500 rounded-full"></span>
+                <span className="text-gray-700">ออกกำลังกาย</span>
+                <TrashIcon className="h-4 w-4 text-gray-500" />
+              </button>
+              <button
+                type="button"
+                className="shadow-md flex items-center gap-2 border-2 bg-white border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                <span className="w-4 h-4 bg-pink-500 rounded-full"></span>
+                <span className="text-gray-700">ทานผลไม้</span>
+                <TrashIcon className="h-4 w-4 text-gray-500" />
+              </button>
+            </div>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2"
+            >
+              <PlusIcon className="h-4 w-5" />
+              เพิ่มนิสัยใหม่
+            </button>
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
